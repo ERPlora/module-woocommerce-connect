78d27aaee7cad38940f7eae6f8eeddeed40230ef
Ioan Beilic <ioanbeilic@gmail.com>
2026-03-28 16:55:04 +0100

    chore: declare HAS_MODELS flag for deterministic install pipeline

--- module.py
+++ module.py
@@ -7,6 +7,7 @@ MODULE_ICON = 'material:store'
 MODULE_DESCRIPTION = _('Sync products, inventory, orders and customers with your WooCommerce store')
 MODULE_AUTHOR = 'ERPlora'
 MODULE_CATEGORY = 'integrations'
+HAS_MODELS = True
 
 MENU = {
     'label': _('WooCommerce'),
